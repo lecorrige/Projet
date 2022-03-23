da60c6a5c5ea5d62ec54edecebd53c0a2c28d7a0
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-23 15:15:10 +0100

    Update Vecteur.h

--- Vecteur.h
+++ Vecteur.h
@@ -1,52 +1,40 @@
 #pragma once
 
-#include <iostream>
 #include <vector>
+#include <ostream>
 
 typedef std::vector<double> Vector;
 
 class Vecteur {
 public:
-
-    Vecteur (unsigned int n) 
-    : vecteur(n)
-    {}
-    Vecteur (double x, double y, double z) 
-    {
-		Vecteur v;
-		v.augmente(x);
-		v.augmente(y);
-		v.augmente(z);
-	}
-	Vecteur (initializer_list <double> const& liste) {
-		for (auto el : liste) { vecteur.push_back(el); }	
-	}
+    Vecteur (unsigned int n=0); 
+    Vecteur (double x, double y, double z);
+	Vecteur (std::initializer_list<double> const& liste);
 
 	void augmente(double valeur) {vecteur.push_back(valeur);}
 	void set_coord(int pos, double valeur) {vecteur[pos] = valeur;}
-	void affiche() const; 
-	bool compare(Vecteur const& v, double precision=1e-10) const;
-	Vector get_vecteur() const;
-	Vecteur operator+=(Vecteur const& v) const;
-	Vecteur operator-=(Vecteur const& v) const;
-	Vecteur operator*=(Vecteur const& v) const;
-	Vecteur operator*=(double k) const;
+	Vector get_vecteur() const {return vecteur;}
+	Vecteur& operator+=(Vecteur const& v);
+	Vecteur& operator-=(Vecteur const& v);
+	Vecteur operator*=(double k);
 	Vecteur operator^(Vecteur const& v) const;
 	double norme() const;
 	double norme2() const;
-	void test_add(Vecteur const& v1) const;
+	bool same_size(Vecteur const& v) const;
 	
 private:
 	Vector vecteur;
-	bool same_size(Vecteur const& v) const;
 	size_t min_size(Vecteur const& v) const;
 	size_t max_size(Vecteur const& v) const;
 };
 
-
-Vecteur operator+(Vecteur v1, Vecteur const& v2) ;
-Vecteur operator*(double k) ;
-Vecteur operator-(Vecteur v1, Vecteur const& v2) ;
-double operator*(Vecteur v1, Vecteur const& v2) ;
-Vecteur operator-(Vecteur const& v) ;
-Vecteur operator~(Vecteur const &v) ;Vecteur operator~(Vecteur const &v) ;
+std::ostream& operator<<(std::ostream& sortie, Vecteur const& v);
+Vecteur operator+(Vecteur v1, Vecteur const& v2);
+Vecteur operator-(Vecteur v1, Vecteur const& v2);
+double operator*(Vecteur const& v1, Vecteur const& v2);
+Vecteur operator*(Vecteur v1, double k);
+Vecteur operator*(double k, Vecteur const& v);
+Vecteur operator-(Vecteur const& v);
+Vecteur operator~(Vecteur const& v);
+bool operator==(Vecteur const& v1, Vecteur const& v2);
+bool operator!=(Vecteur const& v1, Vecteur const& v2);
