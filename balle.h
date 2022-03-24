ff676cc1da2c7429edd190f098413ffb03746f08
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-24 10:13:41 +0100

    Update balle.h

--- balle.h
+++ balle.h
@@ -2,20 +2,31 @@
 
 #include <iostream>
 #include <vector>
+#include <ostream>
 #include "Vecteur.h"
 
 class Balle {
 public:
-
-    //constructeurs
-
-    double masse_volumique();
-    void Balle::ajoute_force(Vecteur const& df);
-
+    Balle (double r=0.0, double rho=0.0, 
+           std::initializer_list<double> const& pos={0, 0, 0}, 
+           std::initializer_list<double> const& vit={0, 0, 0}, 
+           std::initializer_list<double> const& force={0, 0, 0});
+    //constructeurs à voir plus tard en fonction de leur initialisation...
+   // pareil pour copie et destructeur
+    double masse() const;
+    void ajoute_force(Vecteur const& df) {force+=df;};
+    double get_rayon() const {return rayon;}     // accesseurs
+    double get_masseVol() const {return masseVol;}
+    Vecteur get_pos() const {return pos;}
+    Vecteur get_vit() const {return vit;}
+    Vecteur get_force() const {return force;}
 private:
-    Vector pos;
-    Vector vit;
+    Vecteur pos;
+    Vecteur vit;
     double rayon;
-    double masse;
-    Vector force;
+    double masseVol;
+    Vecteur force;
 };
+
+std::ostream& operator<<(std::ostream& sortie, Balle const& b);
+
