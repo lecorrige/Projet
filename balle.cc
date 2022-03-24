d5a5dc94fb0ba1d4bce4c1b83fe602b9073bf684
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-24 10:13:11 +0100

    Update balle.cc

--- balle.cc
+++ balle.cc
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <ostream>
 #include "Vecteur.h"
 #include "balle.h"
 using namespace std;
 
 
-double Balle::masse_volumique() {
-	return (3/(4*M_PI*rayon*rayon*rayon));
+Balle ::Balle(double r, double rho, 
+              std::initializer_list<double> const& pos, 
+              std::initializer_list<double> const& vit, 
+              std::initializer_list<double> const& force) 
+: rayon(r), masseVol(rho), pos(pos), vit(vit), force(force)
+{}
+
+double Balle::masse() const {
+	const double k((M_PI*4)/3);
+	return (masseVol*pow(rayon, 3)*k);
 }
 
-void Balle::ajoute_force(Vecteur const& df) {
-	force += df;
+std::ostream& operator<<(ostream& sortie, Balle const& b) {
+	sortie << b.masse() << " # masse" << endl;
+	sortie << b.get_rayon() << " # rayon" << endl;
+	sortie << b.get_masseVol() << " # masse volumique" << endl;
+	sortie << b.get_pos() << " # position" << endl;
+	sortie << b.get_vit() << " # vitesse" << endl;
+	sortie << b.get_force() << " # force" << endl;
+	return sortie;
 }
