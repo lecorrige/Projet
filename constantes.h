83ae8188f8f819066e2aa939854699199a9345a7
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-24 10:19:24 +0100

    Create constantes.h

---
+++ constantes.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Vecteur.h"
+
+extern const double precision;
+extern const double rho_air;
+extern const std::string erreur_dim;
+extern const std::string erreur_dim3;
+extern const Vecteur g;
