74475084df4ee95644395d0ff17c3b2ae1ba5202
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-03-25 15:44:55 +0100

    Create Balle.cc

---
+++ Balle.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "Vecteur.h"
+#include "balle.h"
+using namespace std;
+
+Balle (double R, double m_vol, Vecteur P, Vecteur dP, Vecteur pos, Vecteur vit, Vecteur F, int deg)
+:ObjetMobile(R, m_vol, P, dP, pos, vit, F, deg)
+{}
+
+
