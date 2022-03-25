af1de86d3584863376ca1d501d8257a640a496d3
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-25 16:23:48 +0100

    Update Balle.cc

--- Balle.cc
+++ Balle.cc
@@ -1,12 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 #include "Vecteur.h"
 #include "balle.h"
+#include "ObjetMobile.h"
 using namespace std;
-
-Balle (double R, double m_vol, Vecteur P, Vecteur dP, Vecteur pos, Vecteur vit, Vecteur F, int deg)
-:ObjetMobile(R, m_vol, P, dP, pos, vit, F, deg)
-{}
 
 
