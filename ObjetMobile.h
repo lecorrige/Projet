fac8119da7c48e128249378a85c9318e2424188e
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-25 16:18:00 +0100

    Update ObjetMobile.h

--- ObjetMobile.h
+++ ObjetMobile.h
@@ -17,34 +17,23 @@ protected:
 	
 	
 public:
+    ObjetMobile (double R, double m, Vecteur pos, Vecteur vit, 
+                 Vecteur P={0}, Vecteur dP={0}, Vecteur F={0, 0, 0});
+	ObjetMobile (double R=0, double m_vol=0, int deg=3); 
+	             
     double masse() const;
-    
     void ajoute_force(Vecteur const& df) {F+=df;};
-	
-	Vecteur evolution();
-	
+	Vecteur evolution() const;
 	void agit_sur(ObjetMobile obj);
-	
-	double distance(const ObjetMobile& obj);
-	
-	ObjetMobile (double R, double m, Vecteur P, Vecteur dP, Vecteur pos, Vecteur vit, Vecteur F);
-	
-	ObjetMobile ( double R=0, double m_vol=0, Vecteur pos={0,0,0}, Vecteur vit={0,0,0}, Vecteur F={0,0,0}, int deg=3);
+	double distance(const ObjetMobile& obj) const;
 	
     double get_rayon() const {return R;}     // accesseurs
-    
     double get_masseVol() const {return m_vol;}
-    
     Vecteur get_pos() const {return pos;}
-    
     Vecteur get_vit() const {return vit;}
-    
     Vecteur get_force() const {return F;}
-    
     Vecteur get_P() const {return P;}
-    
     Vecteur get_dP() const {return dP;}
-    
     void set_f(Vecteur force) {F+=force;}
 };
 
