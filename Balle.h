9bdc298cd8e00065831ad3a97dbe2e38fd9951d4
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-25 16:24:12 +0100

    Update Balle.h

--- Balle.h
+++ Balle.h
@@ -1,18 +1,18 @@
 #pragma once
 
 #include <iostream>
-#include <vector>
 #include "Vecteur.h"
+#include "ObjetMobile.h"
 
-class Balle : public ObjetMobile
-{
+class Balle : public ObjetMobile {
 public:
-	Balle (double R, double m_vol, Vecteur P, Vecteur dP, Vecteur pos, Vecteur vit, Vecteur F, int deg=3);
-	
+	using ObjetMobile::ObjetMobile;     //attention si nouveaux attributs
 	Vecteur get_pos() const { return pos; }  
 	Vecteur get_vit() const { return vit; }
 	
-	void set_pos(Vecteur pos) { pos=p; }
-	void set_vit(Vecteur vit) { vit=v; }
+	void set_pos(Vecteur p) { pos=p; }
+	void set_vit(Vecteur v) { vit=v; }
 
 };
+
+//rajouter operateur affichage
