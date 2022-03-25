b298582a17b5bf1521c6827538760e6b7deba75e
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-25 14:55:45 +0100

    Update constantes.cc

--- constantes.cc
+++ constantes.cc
@@ -1,4 +1,5 @@
- #include "Vecteur.h"
+#include "Vecteur.h"
+#include <cmath>
 using namespace std;
 
 extern const double precision(1e-10);
@@ -6,3 +7,4 @@ extern const double rho_air(1.2);
 extern const string erreur_dim("Les vecteurs ne sont pas de même dimension.");
 extern const string erreur_dim3("Les vecteurs ne sont pas de dimension 3.");
 extern const Vecteur g(0.0, 0.0, -9.81);
+extern const double K((M_PI*4)/3);
