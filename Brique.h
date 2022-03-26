c854b2aab7f8169090c057b49ed3c1b613fea7c1
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-03-26 15:51:14 +0100

    Update Brique.h

--- Brique.h
+++ Brique.h
@@ -1,3 +1,8 @@
+#include "Vecteur.h"
+#include "ObjetMobile.h"
+#include "Obstacle.h"
+#include <vector>
+
 class Brique : public Obstacle
 {
 private:
@@ -8,7 +13,9 @@ private:
 public:
 	Brique (Vecteur pos, Vecteur longueur, Vecteur largeur, double h);
 	
-	Vecteur proche_plan_fini(const ObjetMobile& obj) const;
+	Vecteur proche_plan_fini(const ObjetMobile& obj, Vecteur pos, double L, Vecteur eL, double l, Vecteur el) const;
 	
 	Vecteur point_plus_proche(const ObjetMobile& obj) const;
+	
+	Vecteur min(std::vector<Vecteur> x);
 };
