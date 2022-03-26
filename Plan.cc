680435022de6b67cc260d175aab81e19527fb769
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-03-26 15:52:33 +0100

    Update Plan.cc

--- Plan.cc
+++ Plan.cc
@@ -1,10 +1,13 @@
 #include "Plan.h"
+#include "Vecteur.h"
+#include "Obstacle.h"
+#include "ObjetMobile.h"
 using namespace std;
 
 Plan::Plan(Vecteur O, Vecteur n)
 :Obstacle(O), n(n)
 {
-	if (n!=unitaire(n)) { n=unitaire(n); }
+	if (n!=~n) { n=~n; }
 }
 
 Vecteur Plan::point_plus_proche(const ObjetMobile& obj) const{
