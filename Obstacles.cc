c68c9398016eaaa14794bbe26928f7fde82e0e7c
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-03-29 13:17:41 +0200

    Update Obstacles.cc

--- Obstacles.cc
+++ Obstacles.cc
@@ -1,11 +1,18 @@
 #include "Obstacle.h"
+#include "Vecteur.h"
+#include "ObjetMobile.h"
+#include <iostream>
+using namespace std;
+
+
+Obstacle::Obstacle(Vecteur pos)
+:pos(pos)
+{}
 
 double Obstacle::distance(const ObjetMobile& obj) const{
-	return norme(pos-point_plus_proche(obj))-obj.get_rayon();
+	return (pos-point_plus_proche(obj)).norme()-obj.get_rayon();
 }
  
 void Obstacle::agit_sur(ObjetMobile obj) const{
-}
-
-Vecteur Obstacle::point_plus_proche(const ObjetMobile& obj) const{
+	cout<<"plus tard"<<endl;
 }
