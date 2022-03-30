8b47e331bca08642a14bbe783c0a029ebf1bf543
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-30 14:27:54 +0200

    Update testBalle.cc

--- testBalle.cc
+++ testBalle.cc
@@ -1,28 +1,31 @@
 #include <iostream>
+#include <string>
 #include "ObjetMobile.h"
 #include "balle.h"
 #include "ChampForces.h"
 #include "constantes.h"
+
 using namespace std;
 
 int main() {
+try {
 	ChampForces f;
 	cout << "Nous avons :" << endl;
-	cout << "un champ de force :" << endl;
 	cout << f << endl;
 	
 	Balle b1(0.2, 3, {1, 2, 3}, {0, 0.1, 0.2});
 	Balle b2(0.1, 1.1, {-1, 1.2, 1.3}, {0, 0.1, 0});
 	f.agit_sur(b1);
 	f.agit_sur(b2);
-	cout << "une balle : " << endl;
 	cout << b1 << endl;
-	cout << "et une autre balle : " << endl;
 	cout << b2 << endl;
 	
 	Balle b_nul;
-	cout << "une balle nulle : " << endl;
 	cout << b_nul << endl;
 	
-	return 0;
+	return 0; 
+}
+catch (const string& erreur) {
+	cerr << "Erreur : " << erreur << endl;
+   }
 }
