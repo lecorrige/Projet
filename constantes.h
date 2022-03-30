650b2301119e65bde57bcfa061f59581597e28a5
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-30 14:34:45 +0200

    Update constantes.h

--- constantes.h
+++ constantes.h
@@ -5,5 +5,6 @@ extern const double precision;
 extern const double rho_air;
 extern const std::string erreur_dim;
 extern const std::string erreur_dim3;
+extern const std::string erreur_masse;
 extern const Vecteur g;
 extern const double K;
