41f0d8b4644d498cc8cbb1e049d9922138bf6bba
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-30 14:33:33 +0200

    Update constantes.cc

--- constantes.cc
+++ constantes.cc
@@ -6,5 +6,6 @@ extern const double precision(1e-10);
 extern const double rho_air(1.2);
 extern const string erreur_dim("Les vecteurs ne sont pas de même dimension.");
 extern const string erreur_dim3("Les vecteurs ne sont pas de dimension 3.");
+extern const std::string erreur_masse("masse pas valable !");
 extern const Vecteur g(0.0, 0.0, -9.81);
 extern const double K((M_PI*4)/3);
