d2856f3967f48cb1ac75e1203d481b5789c71d47
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-30 14:28:51 +0200

    Update Balle.cc

--- Balle.cc
+++ Balle.cc
@@ -1,7 +1,21 @@
 #include <iostream>
-#include "Vecteur.h"
 #include "balle.h"
 #include "ObjetMobile.h"
+#include "constantes.h"
 using namespace std;
 
 
+ostream& operator<<(ostream& sortie, Balle const& balle) {
+	const ObjetMobile& o(balle);
+	cout << "une balle : " << endl;
+	sortie << o;
+	sortie << balle.get_P() << " # parametre" << endl;
+	sortie << balle.get_dP() << " # vitesse" << endl;
+	sortie << balle.evolution() << " # f()" << endl;
+	return sortie;
+}
+
+Vecteur Balle::evolution() const {
+	test_masse();         //on teste si masse pas valable
+	return F*(1/masse());
+}
