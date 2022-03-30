9b2b1ac5e647a780efacff66c5d324b79ac49f54
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-30 14:26:37 +0200

    Update ChampForces.cc

--- ChampForces.cc
+++ ChampForces.cc
@@ -2,25 +2,27 @@
 #include <vector>
 #include <cmath>
 #include "Vecteur.h"
-#include "balle.h"
+#include "ObjetMobile.h"
 #include "ChampForces.h"
 #include "constantes.h"
 #include <ostream>
 using namespace std;
 
-ChampForces ::ChampForces(Vecteur v)
-: intensite(v)
+ChampForces ::ChampForces(Vecteur v, bool test)
+: intensite(v), est_ce_que_poussee(test)
 {}
 
 ostream& operator<<(ostream& sortie, ChampForces const& f) {
+	cout << "un champ de force :" << endl;
 	sortie << f.get_intensite() << " # intensite" << endl;
 	return sortie;
 }
 
-void ChampForces::agit_sur(Balle& balle){
-	double k(balle.masse()-4*M_PI*rho_air*pow(balle.get_rayon(), 3)/3);
-	Vecteur force(k*intensite);
-	balle.ajoute_force(force);
+void ChampForces::agit_sur(ObjetMobile& obj) const {
+	double k;
+	Vecteur v(3);
+	if (obj.get_F() == v) {obj.set_F(intensite.get_vecteur().size());}
+	if (est_ce_que_poussee) {k=(obj.masse()-4*M_PI*rho_air*pow(obj.get_rayon(), 3)/3);}
+	else {k=obj.masse();}
+	obj.ajoute_force(k*intensite);
 }
-
-	
