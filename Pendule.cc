36d721ab5ce3ffa5473fa8c81dc7ae52d88b98eb
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-03-30 14:25:58 +0200

    Update Pendule.cc

--- Pendule.cc
+++ Pendule.cc
@@ -3,36 +3,45 @@
 #include "Vecteur.h"
 #include "Pendule.h"
 #include "constantes.h"
+#include <cmath>
 
 using namespace std;
 
 
 Vecteur Pendule::evolution() const { 
-	Vecteur theta_2_point((cos(P.norme())*(F*d)-sin(P.norme())*F*(~g)-(frottement*dP.norme())/L)/(masse()*L)) ;
-	
-	/*Vecteur v1{0,0,cos(P)};
-	pos_bout=O+L*v1+L*sin(P)*d;
-	
-	Vecteur v2{0,0,-sin(P)};
-	vit=m*L*(theta_2_point*(cos(P)*d-v2)-pow(dP,2)*(sin(theta)*d+v1));*/
-	
-	return theta_2_point;
+	return {(cos(P.get_vecteur()[0])*F*d - sin(P.get_vecteur()[0])*F*(~g) 
+	         - b*dP.get_vecteur()[0]/L)*(1/(masse()*L))} ;
 }
 
-Pendule::Pendule(double R, double m_vol, Vecteur P, Vecteur dP, Vecteur pos, Vecteur vit, Vecteur F,
-				 double L, double frott, Vecteur d, Vecteur pos_bout)
-				 
-	:ObjetMobile (R, m_vol, P, dP, pos, vit, F), 
-	 L(L), frottement(frott), d(d), pos_bout(pos_bout)
+Pendule::Pendule (double R, double m_vol, double L, double b, Vecteur P, Vecteur dP, Vecteur d, Vecteur O)
+: ObjetMobile (R, m_vol, P, dP, b), L(L), O(O), d(d)
 {}
 
-ostream& operator<<(ostream& sortie,  Pendule const& p){
-	ObjetMobile& o(p);
-	sortie<<o;
+Pendule::Pendule (double R, double m_vol, double L, double b, int deg)
+: ObjetMobile (R, m_vol, b, deg), L(L), O(3), d(3)
+{} 
+
+ostream& operator<<(ostream& sortie, Pendule const& p){
 	sortie<<"un pendule "<<endl;
-	sortie<<p.get_fr()<<" # frottement pendule"<<endl;
+	sortie<<p.masse()<<" # masse pendule"<<endl;
+	sortie<<p.get_L()<<" # longueur pendule"<<endl;
+	sortie<<p.get_b()<<" # frottement pendule"<<endl;
+	sortie<<p.get_O()<<" # origine pendule" << endl;
 	sortie<<p.get_d()<<" # direction pendule"<<endl;
-	sortie<<p.get_pos_bout()<<" # position bout pendule"<<endl;
+	sortie<<p.get_P()<<" # theta pendule" << endl;
+	sortie<<p.get_dP()<<" # theta point pendule"<<endl;
+	sortie<<p.evolution()<<" # f() pendule" << endl;
+	sortie<<p.pos_masse()<<" # position bout pendule"<<endl;
+	sortie<<p.vit_masse()<<" # vitesse bout pendule"<<endl;
+	sortie<<p.get_F()<<" # force exercee sur le pendule"<<endl;
 	
 	return sortie; 
+}
+
+Vecteur Pendule::pos_masse() const {
+	return (O + L*cos(P.get_vecteur()[0])*(~g) + L*sin(P.get_vecteur()[0])*d);
+}
+
+Vecteur Pendule::vit_masse() const {
+	return (L*dP.get_vecteur()[0]*(cos(P.get_vecteur()[0])*d - sin(P.get_vecteur()[0])*(~g)));
 }
