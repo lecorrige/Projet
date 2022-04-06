b63593897ccc5d422d7321c3afb5733854633210
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-04-06 16:08:59 +0200

    Update Brique.h

--- Brique.h
+++ Brique.h
@@ -1,21 +1,21 @@
 #include "Vecteur.h"
 #include "ObjetMobile.h"
-#include "Obstacle.h"
+#include "Obstacles.h"
 #include <vector>
+#include <memory>
 
-class Brique : public Obstacle
-{
+class Brique : public Obstacle {
 private:
-	Vecteur L;
-	Vecteur l;
-	double h;
+	Vecteur L;     //longueur
+	Vecteur l;     //largeur
+	double h;      //hauteur
+	Vecteur min(std::vector<Vecteur> const&, const ObjetMobile&) const;
+	Vecteur proche_plan_fini(const ObjetMobile& obj, Vecteur pos, double L, 
+	                         Vecteur eL, double l, Vecteur el) const;
 
 public:
-	Brique (Vecteur pos, Vecteur longueur, Vecteur largeur, double h);
+	Brique (Vecteur pos=(3), Vecteur longueur=(3), Vecteur largeur=(3), double h=0);
 	
-	Vecteur proche_plan_fini(const ObjetMobile& obj, Vecteur pos, double L, Vecteur eL, double l, Vecteur el) const;
-	
-	Vecteur point_plus_proche(const ObjetMobile& obj) const;
-	
-	Vecteur min(std::vector<Vecteur> x);
+	virtual Vecteur point_plus_proche(const ObjetMobile& obj) const override;
+	virtual std::ostream& affiche(std::ostream&) const override;
 };
