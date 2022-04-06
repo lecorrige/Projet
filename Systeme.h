b3b4d14354997a41628a2d5f8af418a209a306e1
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-04-06 17:20:54 +0200

    Update Systeme.h

--- Systeme.h
+++ Systeme.h
@@ -10,12 +10,14 @@
 class Systeme{
 public:
     Systeme (std::vector<ObjetMobile*> const& obj,
-	         std::vector<Obstacle*> const& obs, ChampForces); //rajouter integrateur
+	         std::vector<Obstacle*> const& obs, ChampForces, 
+	         Integrateur*); //rajouter integrateur
 	ChampForces get_champ() const {return *champ;}
 	std::ostream& afficher_obj(std::ostream& sortie) const;
 	std::ostream& afficher_obs(std::ostream& sortie) const;
 	void ajouter_objets (vector<ObjetMobile*> const& obj);
 	void ajouter_objets (vector<Obstacle*> const& obj);
+	void evolue();
 	
 private:
 	std::vector<std::unique_ptr<ObjetMobile>> objets;
