8371db6f6c17dcd776cb0935d066f3437bed9898
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-04-06 15:54:51 +0200

    Update Pendule.h

--- Pendule.h
+++ Pendule.h
@@ -15,16 +15,17 @@ public:
              Vecteur d, Vecteur O=(3));
     Pendule (double R=0, double m_vol=0, double L=0, double b=0, int deg=1); 
 
-	Vecteur evolution() const; 
+	virtual Vecteur evolution() const override; 
 	double get_L() const { return L; }
 	Vecteur get_O() const {return O;}
 	Vecteur get_d() const {return d;}
 	Vecteur pos_masse() const;    //calcule la position du bout du pendule
 	Vecteur vit_masse() const;    //calcule la vitesse du bout du pendule
+	Vecteur force_masse() const;  //calcule la force reelle exercee sur le bout du pendule (y compris force centripete)
 	
 	virtual Vecteur pos() const override {return pos_masse();}  
 	virtual Vecteur vit() const override {return vit_masse();}
-	virtual void ajoute_vit(Vecteur v) const override {dP+=v/L}	
-	virtual ostream& affiche(ostream& sortie) const override;
+	virtual void ajoute_vit(Vecteur const& v) override {dP+=v/L;}	
+	virtual std::ostream& affiche(std::ostream& sortie) const override;
 };
 
