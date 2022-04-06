e974769ee0f54eb2e747a1f3177c5d4a06f823b6
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-04-06 16:14:02 +0200

    Update Systeme.h

--- Systeme.h
+++ Systeme.h
@@ -4,20 +4,24 @@
 #include <memory>
 #include "ObjetMobile.h"
 #include "Integrateur.h"
-#include "Obstacle.h"
+#include "Obstacles.h"
 #include "ChampForces.h"
 
 class Systeme{
 public:
-	ChampForces get_champ() {return champ;}
-	std::ostream& afficher_obj(std::ostream& sortie);
-	std::ostream& afficher_obs(std::ostream& sortie);
+    Systeme (std::vector<ObjetMobile*> const& obj,
+	         std::vector<Obstacle*> const& obs, ChampForces); //rajouter integrateur
+	ChampForces get_champ() const {return *champ;}
+	std::ostream& afficher_obj(std::ostream& sortie) const;
+	std::ostream& afficher_obs(std::ostream& sortie) const;
+	void ajouter_objets (vector<ObjetMobile*> const& obj);
+	void ajouter_objets (vector<Obstacle*> const& obj);
 	
 private:
 	std::vector<std::unique_ptr<ObjetMobile>> objets;
 	std::vector<std::unique_ptr<Obstacle>> obstacles;
-	Integrateur integre;
-	ChampForces champ;
+	std::unique_ptr<ChampForces> champ;
+	std::unique_ptr<Integrateur> integre;
 };
 
-std::iostream& operator<<(std::iostream& sortie, Systeme const& syst);
+std::ostream& operator<<(std::ostream& sortie, Systeme const& syst);
