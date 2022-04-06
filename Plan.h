f9bf8aa3321b9d6f3bd49265fb9e460978294321
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-04-06 16:11:31 +0200

    Update Plan.h

--- Plan.h
+++ Plan.h
@@ -1,13 +1,13 @@
-#include "Obstacle.h"
+#include "Obstacles.h"
 #include "Vecteur.h"
 #include "ObjetMobile.h"
 
-class Plan : public Obstacle
-{
+class Plan : public Obstacle {
 private:
 	Vecteur n;
 public:
-	Plan(Vecteur O, Vecteur n);
+	Plan(Vecteur pos=(3), Vecteur n=(3));
 
-	Vecteur point_plus_proche(const ObjetMobile& obj) const;
+	virtual Vecteur point_plus_proche(const ObjetMobile&) const override;
+	virtual std::ostream& affiche(std::ostream&) const override;
 };
