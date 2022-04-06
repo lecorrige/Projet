9feb219d4022af6963b076adc0dd52ab8ac231f7
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-04-06 16:06:41 +0200

    Update Obstacles.h

--- Obstacles.h
+++ Obstacles.h
@@ -1,18 +1,20 @@
+#pragma once
+
 #include "Vecteur.h"
 #include "ObjetMobile.h"
 
-class Obstacle
-{
+class Obstacle {
 protected:
 	Vecteur pos;
 
 public:
-	 Obstacle (Vecteur pos);
-
-	 double distance(const ObjetMobile& obj) const;
+	 Obstacle (Vecteur pos=(3));
 	 
-	 void agit_sur(ObjetMobile obj) const;
+	 double distance(const ObjetMobile&) const;
+	 void agit_sur(ObjetMobile&) const;
 	 
-	 virtual Vecteur point_plus_proche(const ObjetMobile& obj)=0 const;
-
+	 virtual Vecteur point_plus_proche(const ObjetMobile&) const =0;
+	 virtual std::ostream& affiche(std::ostream&) const =0;
 };
+
+std::ostream& operator<<(std::ostream&, Obstacle const&);
