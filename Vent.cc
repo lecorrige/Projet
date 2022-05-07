ce5cca8e971383859a678eeda27d03ecc0be5071
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-05-07 13:20:32 +0200

    Update Vent.cc

--- Vent.cc
+++ Vent.cc
@@ -2,17 +2,10 @@
 #include "Vecteur.h"
 #include "Objet.h"
 
-Vent(Vecteur o, Vecteur n, double l, double L,
-		 double p, Vecteur v=g, bool p=true)
-:ChampForces(v,p),origine(o), normale(n), 
+Vent::Vent(Vecteur o, Vecteur n, double l, double L,
+		   double p, Vecteur v, bool po)
+:ChampForces(v,po),origine(o), normale(n), 
  largeur(l), longueur(L), profondeur(p)
 {}
 
-bool Vent::est_dans(ObjetMobile const& obj) const{
-	return ((((origine-obj.pos())*normale)>0) and 
-			(((origine-obj.pos())*normale)<profondeur) and
-			(((origine-obj.pos())*~largeur)>0) and
-			(((origine-obj.pos())*~largeur)<largeur) and
-			(((origine-obj.pos())*~longueur)>0) and
-			(((origine-obj.pos())*~longueur)<longueur));
-}
+
