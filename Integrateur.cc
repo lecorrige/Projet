3ce287926fb03e9d6951117a875eb791a6733111
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:29:28 +0200

    Update Integrateur.cc

--- Integrateur.cc
+++ Integrateur.cc
@@ -15,7 +15,23 @@ IntegrateurEulerCromer::IntegrateurEulerCromer (double dt)
 
 
 void IntegrateurEulerCromer::evolue(ObjetMobile& obj) const { 
-   obj.set_dP(obj.get_dP() + dt*obj.evolution());                      //regler pb eq dif ordre 1 ou 2
-   obj.set_P(obj.get_P() + dt*obj.get_dP()); 
+   obj.set_vit(obj.get_dP() + dt*obj.evolution());                     
+   obj.set_pos(obj.get_P() + dt*obj.get_dP()); 
 }
 
+IntegrateurEulerCromer* IntegrateurEulerCromer::copie() const {
+	return new IntegrateurEulerCromer(*this);
+}
+
+ostream& operator<<(ostream& sortie, Integrateur const& integ) {
+	integ.affiche_nom(sortie);
+	sortie<<"dt = "
+		  <<integ.get_dt()
+		  <<endl;
+	return sortie;
+}
+
+ostream& IntegrateurEulerCromer::affiche_nom(ostream& sortie) const { 
+	sortie<<"Integrateur Euler-Cromer"<<endl;
+	return sortie; 
+}
