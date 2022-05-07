d65e270cb0f0f0a422f2656f14e8b8c49da764a0
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:32:53 +0200

    Update Obstacles.cc

--- Obstacles.cc
+++ Obstacles.cc
@@ -18,25 +18,47 @@ ostream& operator<<(ostream& sortie, Obstacle const& obs) {
 	return obs.affiche(sortie);
 }
 
-void Obstacle::agit_sur(ObjetMobile& obj) const{
-	Vecteur n(~(pos-obj.pos()));
+void Obstacle::agit_sur(ObjetMobile& obj, bool afficher) {
+	Vecteur n(normale(obj));
 	double lambda(1+alpha);
 	
 	if (distance(obj)<=obj.get_rayon()){
 		double F1(obj.get_F()*n);
-		obj.ajoute_force(abs(F1)*n);
+		if (F1<0) {obj.ajoute_force(-F1*n);}
 		
-		double v_star((-obj.vit())*n);
+		double v_star(-(obj.vit()*n));
+		double coeff(7*nu*lambda*v_star);
 		Vecteur vc(obj.vit()+v_star*n);
 		Vecteur delta_v;
 		
-		if ((7*nu*(1+alpha)*v_star)>=2*vc.norme()){
-			delta_v=lambda*v_star*n-2/7*vc;
+		if (coeff>=2*vc.norme()){
+			delta_v=lambda*v_star*n-(2/7)*vc;
 		}
 		else {
 			delta_v=lambda*v_star*(n-nu*(~vc));
 		}
 		
+		if (afficher) {
+			cout << "calculs :" << endl;
+			cout << "  n = " << n << endl;
+			cout << "  vstar = " << v_star << endl;
+			cout << "  vc = " << vc << endl;
+			cout << "  7 mu (1+alpha) vstar = " << coeff << endl;
+			cout << "  dv = " << delta_v << endl;
+		}
+		
 		obj.ajoute_vit(delta_v);
 	}
 }
+
+void Obstacle::test_choc(ObjetMobile& obj) {
+	cout << "avant choc : " << endl;
+	cout << "      vitesse = " << obj.vit() << endl;
+	cout << "      force = " << obj.get_F() << endl << endl;
+	agit_sur(obj, true);
+	cout << endl << "après choc : " << endl;
+	cout << "      vitesse = " << obj.vit() << endl;
+	cout << "      force = " << obj.get_F() << endl << endl;
+}
+	
+	
