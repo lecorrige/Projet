fbe59833177aec5d07d790fdcfbfc46d40981f01
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-05-07 14:21:08 +0200

    Update Ventilateur.cc

--- Ventilateur.cc
+++ Ventilateur.cc
@@ -2,17 +2,18 @@
 #include "constantes.h"
 #include <cmath>
 
-Ventilateur::Ventilateur(Vent const& v, Brique const& b)
-:Compose({&v, &b})
+Ventilateur::Ventilateur(Vent* v, Brique* b)
+:Compose({v, b})
 {
-	if (v.get_O()!=b.get_O()){ objets[0]->set_O(b.get_O()); }
-	if (v.get_l()!=b.get_l()){ objets[0]->set_l(b.get_l()); }
-	if (v.get_L()!=b.get_L()){ objets[0]->set_L(b.get_L()); }
+	if (v->get_O()!=b->get_O()){ objets[0]->set_O(b->get_O()); }
+	if (v->get_l()!=b->get_l()){ objets[0]->set_l(b->get_l()); }
+	if (v->get_L()!=b->get_L()){ objets[0]->set_L(b->get_L()); }
 }
 
 void Ventilateur::agit_sur(ObjetMobile& obj) {
 	if (est_dans(obj)){
-		Vecteur F(Q*pow(objets[0].get_intensite()[0],2)*obj.get_rayon()*objets[0]->get_n());
+		Vecteur F(Q*pow(objets[0].get_intensite()[0],2)
+				  *obj.get_rayon()*objets[0]->get_n());
 		obj.ajoute_force(F);
 	}
 }
