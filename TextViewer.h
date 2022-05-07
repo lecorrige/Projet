06d2e7e732aae399f8b5cd9a709cc4b98a3d8648
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:40:12 +0200

    Update TextViewer.h

--- TextViewer.h
+++ TextViewer.h
@@ -1,11 +1,32 @@
 #pragma once
 
 #include "SupportADessin.h"
+#include "Systeme.h"
+#include "balle.h"
+#include "Pendule.h"
+#include <ostream>
+#include <iostream>
+
 
 class TextViewer : public SupportADessin
 {
 public:	
-	~SupportADessin() override;
-	void dessine(Systeme const& syst) override;
-	void dessine(Balle const& ball) override;
+    TextViewer() =default;
+	virtual ~TextViewer() = default;
+	 // on ne copie pas les TextViewer
+    TextViewer(TextViewer const&)            = delete; 
+    TextViewer& operator=(TextViewer const&) = delete;
+     // mais on peut les déplacer
+    TextViewer(TextViewer&&)            = default; 
+    TextViewer& operator=(TextViewer&&) = default;
+	
+	virtual void dessine(Systeme const&) override;
+	virtual void dessine(Balle const&) override;
+	virtual void dessine(Pendule const&) override;
+    void param(Systeme const&);
+	void param(Balle const&);
+	void param(Pendule const&);
+
 };
+
+void test_systeme(Systeme& syst, TextViewer& text, int rep);
