7c44d989d0a7f106708cdafbcbaaac4b57be6b47
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-05-07 13:19:57 +0200

    Update Ventilateur.cc

--- Ventilateur.cc
+++ Ventilateur.cc
@@ -2,7 +2,7 @@
 #include "constantes.h"
 #include <cmath>
 
-Ventilateur(Vent const& v, Brique const& b)
+Ventilateur::Ventilateur(Vent const& v, Brique const& b)
 :Compose({&v, &b})
 {
 	if (v.get_O()!=b.get_O()){ objets[0]->set_O(b.get_O()); }
@@ -10,9 +10,18 @@ Ventilateur(Vent const& v, Brique const& b)
 	if (v.get_L()!=b.get_L()){ objets[0]->set_L(b.get_L()); }
 }
 
-void agit_sur(ObjetMobile& obj) {
-	if (objets[0]->est_dans(obj){
-		Vecteur F(Q*pow(objets[0].get_intensite()[0],2)*obj.get_rayon()*normale);
+void Ventilateur::agit_sur(ObjetMobile& obj) {
+	if (est_dans(obj)){
+		Vecteur F(Q*pow(objets[0].get_intensite()[0],2)*obj.get_rayon()*objets[0]->get_n());
 		obj.ajoute_force(F);
 	}
+}
+
+bool Ventilateur::est_dans(ObjetMobile const& obj) const{
+	return ((((objets[0]->get_O()-obj.pos())*objets[0]->get_n())>0) and 
+			(((objets[0]->get_O()-obj.pos())*objets[0]->get_n())<objets[0]->get_p()) and
+			(((objets[0]->get_O()-obj.pos())*(~(objets[1]->get_l()))>0) and
+			(((objets[0]->get_O()-obj.pos())*(~(objets[1]->get_l())))<<objets[0]->get_L()) and
+			(((objets[0]->get_O()-obj.pos())*(~(objets[1]->get_L())))>0) and
+			(((objets[0]->get_O()-obj.pos())*(~(objets[1]->get_L())))<objets[0]->get_l())));
 }
