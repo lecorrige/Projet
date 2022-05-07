40db8a0f79c36e6dae6f109d2317a58e8144620a
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:33:24 +0200

    Update Obstacles.h

--- Obstacles.h
+++ Obstacles.h
@@ -6,15 +6,19 @@
 class Obstacle {
 protected:
 	Vecteur pos;
-
+	
+	virtual Vecteur normale(const ObjetMobile&) const =0;
 public:
-	 Obstacle (Vecteur pos=(3));
+	Obstacle (Vecteur pos=(3));
 	 
-	 double distance(const ObjetMobile&) const;
-	 void agit_sur(ObjetMobile&) const;
+    double distance(const ObjetMobile&) const;
+	void agit_sur(ObjetMobile&, bool=false);
 	 
-	 virtual Vecteur point_plus_proche(const ObjetMobile&) const =0;
-	 virtual std::ostream& affiche(std::ostream&) const =0;
+	virtual Vecteur point_plus_proche(const ObjetMobile&) const =0;
+	virtual std::ostream& affiche(std::ostream&) const =0;
+	virtual Obstacle* copie() const =0;
+	void test_choc(ObjetMobile&);
+	Vecteur get_O() const {return pos;}
 };
 
 std::ostream& operator<<(std::ostream&, Obstacle const&);
