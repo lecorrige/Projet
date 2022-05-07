4d38d7108dcb06a5310e313fb7d07466b9d0ba1f
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-05-07 13:20:56 +0200

    Update Vent.h

--- Vent.h
+++ Vent.h
@@ -5,15 +5,16 @@
 class Vent : public ChampForces {
 public:
 	Vent(Vecteur o, Vecteur n, double l, double L,
-		 double p, Vecteur v=g, bool p=true);
+		 double p, Vecteur v=g, bool po=true);
 		 
-	bool est_dans(Objet const& obj) const;
+	bool est_dans(ObjetMobile const& obj) const;
 	double get_l() const { return largeur; }
 	double get_L() const { return longueur; }
+	double get_p() const { return profondeur; }
 	Vecteur get_O() const { return origine; }
 	Vecteur get_n() const { return normale; }
-	void set_l(double x) { l=x; }
-	void set_L(double x) { L=x; }
+	void set_l(double x) { largeur=x; }
+	void set_L(double x) { longueur=x; }
 	void set_O(double x) { origine=x; }
 	
 private:
