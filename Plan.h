42a9866bdebc82359e9c6361e147ea349c030c54
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:35:27 +0200

    Update Plan.h

--- Plan.h
+++ Plan.h
@@ -5,9 +5,13 @@
 class Plan : public Obstacle {
 private:
 	Vecteur n;
+	
+	virtual Vecteur normale(const ObjetMobile&) const override {return n;}
 public:
 	Plan(Vecteur pos=(3), Vecteur n=(3));
 
 	virtual Vecteur point_plus_proche(const ObjetMobile&) const override;
 	virtual std::ostream& affiche(std::ostream&) const override;
+	virtual Plan* copie() const override;
+	Vecteur get_n() const {return n;}
 };
