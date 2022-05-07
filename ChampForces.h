645ec5ecfc8d2ec451aa7baafc889690ca8490a1
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:26:57 +0200

    Update ChampForces.h

--- ChampForces.h
+++ ChampForces.h
@@ -7,7 +7,7 @@
 class ChampForces{
 public:
     ChampForces (Vecteur v=g, bool test=true);   //constructeur avec pour valeur par défaut g(gravite)
-	void agit_sur(ObjetMobile& obj) const;
+	void agit_sur(ObjetMobile& obj);
 	Vecteur get_intensite() const {return intensite;}
 private:
 	Vecteur intensite;    //représente direction et intensité(norme) du champ de force
