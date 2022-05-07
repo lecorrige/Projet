7ef0fa8223dfb3a2df82805dd03cdecc9d461b30
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:33:57 +0200

    Update Pendule.cc

--- Pendule.cc
+++ Pendule.cc
@@ -15,11 +15,15 @@ Vecteur Pendule::evolution() const {
 
 Pendule::Pendule (double R, double m_vol, double L, double b, Vecteur P, Vecteur dP, Vecteur d, Vecteur O)
 : ObjetMobile (R, m_vol, P, dP, b), L(L), O(O), d(d)
-{} //verifier d orthogonal à g
+{F=(pos().get_vecteur().size());
+ test_orth(); 
+ test_tige();                  }
 
 Pendule::Pendule (double R, double m_vol, double L, double b, int deg)
 : ObjetMobile (R, m_vol, b, deg), L(L), O(3), d(3)
-{} 
+{F=(pos().get_vecteur().size());
+ test_orth();
+ test_tige();                   } 
 
 ostream& Pendule::affiche(ostream& sortie) const{
 	sortie<<"un pendule "<<endl;
@@ -50,4 +54,20 @@ Vecteur Pendule::force_masse() const {
 	Vecteur x1(evolution().get_vecteur()[0]*(cos(P.get_vecteur()[0])*d-sin(P.get_vecteur()[0])*(~g)));
 	Vecteur x2(pow(dP.get_vecteur()[0], 2)*(sin(P.get_vecteur()[0])*d+cos(P.get_vecteur()[0])*(~g)));
 	return masse()*L*(x1-x2);
+}
+
+Pendule* Pendule::copie() const {
+	return new Pendule(*this);
+}
+
+void Pendule::test_orth() const {
+	if (g*d != 0) {throw erreur_orth;}
+}
+
+void Pendule::test_tige() const {
+	if (L-0<precision) {throw erreur_tige;}
+}
+
+void Pendule::ajoute_force(Vecteur const& df) {
+	F += df;
 }
