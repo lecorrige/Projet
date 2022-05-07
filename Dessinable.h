e6cdd0769494d6fd4b8d761bbfd74e7043120cd6
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:28:49 +0200

    Update Dessinable.h

--- Dessinable.h
+++ Dessinable.h
@@ -1,6 +1,16 @@
+#pragma once
+
 #include "SupportADessin.h"
 
 class Dessinable{
 public:
-	virtual void dessine_sur(SupportADessin& support) =0;
+	virtual ~Dessinable()=default;
+	Dessinable()                             = default;
+	Dessinable(Dessinable const&)            = default;
+    Dessinable& operator=(Dessinable const&) = default;
+    Dessinable(Dessinable&&)                 = default; 
+    Dessinable& operator=(Dessinable&&)      = default;
+	
+	virtual void dessine_sur(SupportADessin&) =0;
+	virtual void param_sur(SupportADessin&) =0;
 };
