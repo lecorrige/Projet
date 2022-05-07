dde5aa14e1a94877c7c1b5e1187440636f1f06fd
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:30:01 +0200

    Update Integrateur.h

--- Integrateur.h
+++ Integrateur.h
@@ -7,11 +7,18 @@
 using namespace std;
 
 
-class Integrateur {       //à expliquer plus tard
+class Integrateur {    
 public:
    Integrateur (double dt=0);
+   virtual ~Integrateur()                     = default;
+   Integrateur(const Integrateur&)            = default;
+   Integrateur& operator=(const Integrateur&) = default;
+   Integrateur(Integrateur&&)                 = default;
+   Integrateur& operator=(Integrateur&&)      = default;
    virtual void evolue(ObjetMobile& obj) const =0;
    double get_dt() const {return dt;}
+   virtual Integrateur* copie() const =0;
+   virtual std::ostream& affiche_nom(std::ostream&) const =0;
 protected:
    double dt;            //pas d'integration        
 };
@@ -21,5 +28,9 @@ protected:
 class IntegrateurEulerCromer : public Integrateur {     //ne pas oublier de faire 
 public:                                                 //avancer le temps du systeme
    IntegrateurEulerCromer (double dt=0);
-   virtual void evolue(ObjetMobile& obj) const override;                  //regler pb eq dif ordre 1 ou 2
+   virtual void evolue(ObjetMobile& obj) const override; //regler pb eq dif ordre 1 ou 2
+   virtual IntegrateurEulerCromer* copie() const override;
+   virtual std::ostream& affiche_nom(std::ostream&) const override;
 };
+
+std::ostream& operator<<(std::ostream&, Integrateur const&);
