c07e54a5e74207ff31e6607b3bed258331b134f9
Leprogrammeurpro <101654220+Leprogrammeurpro@users.noreply.github.com>
2022-05-07 13:31:44 +0200

    Update ObjetMobile.cc

--- ObjetMobile.cc
+++ ObjetMobile.cc
@@ -11,15 +11,21 @@ double ObjetMobile::masse() const {   //creer fonction inverse en fonction rayon
 	return (m_vol*pow(R, 3)*K);
 }
 
-void ObjetMobile::agit_sur(ObjetMobile& obj){
+void ObjetMobile::agit_sur(ObjetMobile& obj, bool test) {
 	Vecteur n(~(pos()-obj.pos()));
 	double lambda(((1+alpha)*obj.masse())/(masse()+obj.masse()));
 	
 	if (distance(obj)<=R+obj.get_rayon()){
 		double F1(F*n);
 		double F2(obj.get_F()*n);
-		ajoute_force((abs(F1)+abs(F2))*n);
-		obj.ajoute_force(-(abs(F1)+abs(F2))*n);	
+		if (F1 < 0) {
+			ajoute_force(-F1*n);
+			obj.ajoute_force(F1*n);
+		}
+		if (F2 > 0) {
+			ajoute_force(F2*n);
+			obj.ajoute_force(-F2*n);
+		}
 		
 		double v_star((obj.vit()-vit())*n);
 		Vecteur vc(vit()-obj.vit()+v_star*n);
@@ -34,6 +40,18 @@ void ObjetMobile::agit_sur(ObjetMobile& obj){
 		
 		ajoute_vit(delta_v);
 		obj.ajoute_vit(-(masse()/obj.masse())*delta_v);
+		
+		if (test) {
+			cout << "   n = " << n << endl;
+			cout << "   lambda = " << lambda << endl;
+			cout << "   F1n = " << F1 << endl;
+			cout << "   F2n = " << F2 << endl;
+			cout << "   vstar = " << v_star << endl;
+			cout << "   vc = " << vc << endl;
+			cout << "   7 mu (1+alpha) vstar = " << 7*nu*(1+alpha)*v_star << endl;
+			cout << "   2*vc.norme() = " << 2*vc.norme() << endl;
+			cout << "   dv = " << delta_v << endl;
+		}
 	}
 }	
 
@@ -41,22 +59,16 @@ double ObjetMobile::distance(const ObjetMobile& obj) const {
 	return (pos()-obj.pos()).norme()-(R+obj.get_rayon());
 }
 
-//void dessine_sur(SupportADessin& support) 
- //  {support.dessine(*this);}
-
 ObjetMobile::ObjetMobile (double R, double m_vol, Vecteur P, Vecteur dP, double b) 
-:R(R), m_vol(m_vol), b(b), P(P), dP(dP), F(3)
-{ test_size();}
-
-ObjetMobile::ObjetMobile (Vecteur P, Vecteur dP)
-:R(0), m_vol(0), b(0), P(P), dP(dP), F(3)
-{test_size();}	
+:R(R), m_vol(m_vol), b(b), P(P), dP(dP)
+{ test_size();
+  F=(pos().get_vecteur().size());}
 	
 ObjetMobile::ObjetMobile (double R, double m_vol, double b, int deg)
-:R(R), m_vol(m_vol), b(b), P(deg), dP(deg), F(3)
+:R(R), m_vol(m_vol), b(b), P(deg), dP(deg), F(deg)
 {}
 
-ostream& ObjetMobile::affiche(ostream& sortie) const{  //voir surcharge affichage
+ostream& ObjetMobile::affiche(ostream& sortie) const{  
 	sortie << masse() << " # masse" << endl;
 	sortie << get_rayon() << " # rayon" << endl;
 	sortie << get_masseVol() << " # masse volumique" << endl;
@@ -75,4 +87,24 @@ void ObjetMobile::test_size() const {
 
 ostream& operator<<(ostream& sortie,  ObjetMobile const& obj){
 	return obj.affiche(sortie);
+}
+
+void ObjetMobile::test_choc(ObjetMobile& ball) {
+	cout << "avant choc:" << endl;
+	cout << "   position balle1 = " << pos() << endl;
+	cout << "   position balle2 = " << ball.pos() << endl;
+	cout << "   distance (entre centres) = " << (P-ball.pos()).norme() << endl;
+	cout << "   vitesse  balle1 = " << vit() << endl;
+	cout << "   vitesse  balle2 = " << ball.vit() << endl;
+	cout << "   force    balle1 = " << F << endl;
+	cout << "   force    balle2 = " << ball.get_F() << endl;
+	
+	cout << endl << "calculs : " << endl;
+	agit_sur(ball, true);
+	
+	cout << "après choc:" << endl;
+	cout << "   vitesse  balle1 = " << vit() << endl;
+	cout << "   vitesse  balle2 = " << ball.vit() << endl;
+	cout << "   force    balle1 = " << F << endl;
+	cout << "   force    balle2 = " << ball.get_F() << endl;
 }
