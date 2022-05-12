8bc683b69274ffc29d452efaf46f5dd6991b1719
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-05-12 10:23:36 +0200

    Update Integrateur.cc

--- Integrateur.cc
+++ Integrateur.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "Integrateur.h"
 #include "ObjetMobile.h"
-#include "balle.h"
 using namespace std;
 
 
@@ -9,19 +8,6 @@ Integrateur::Integrateur (double dt)
 : dt(dt)
 {}
 
-IntegrateurEulerCromer::IntegrateurEulerCromer (double dt) 
-: Integrateur(dt)
-{}
-
-
-void IntegrateurEulerCromer::evolue(ObjetMobile& obj) const { 
-   obj.set_vit(obj.get_dP() + dt*obj.evolution());                     
-   obj.set_pos(obj.get_P() + dt*obj.get_dP()); 
-}
-
-IntegrateurEulerCromer* IntegrateurEulerCromer::copie() const {
-	return new IntegrateurEulerCromer(*this);
-}
 
 ostream& operator<<(ostream& sortie, Integrateur const& integ) {
 	integ.affiche_nom(sortie);
@@ -31,7 +17,3 @@ ostream& operator<<(ostream& sortie, Integrateur const& integ) {
 	return sortie;
 }
 
-ostream& IntegrateurEulerCromer::affiche_nom(ostream& sortie) const { 
-	sortie<<"Integrateur Euler-Cromer"<<endl;
-	return sortie; 
-}
