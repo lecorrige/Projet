a80ff3d0e4f9aba44eec3a28b9056904a9fc58c2
lecorrige <101654222+lecorrige@users.noreply.github.com>
2022-05-12 10:23:06 +0200

    Update Integrateur.h

--- Integrateur.h
+++ Integrateur.h
@@ -2,9 +2,6 @@
 
 #include <iostream>
 #include "ObjetMobile.h"
-#include "balle.h"
-#include "Pendule.h"
-using namespace std;
 
 
 class Integrateur {    
@@ -23,14 +20,5 @@ protected:
    double dt;            //pas d'integration        
 };
 
-
-
-class IntegrateurEulerCromer : public Integrateur {     //ne pas oublier de faire 
-public:                                                 //avancer le temps du systeme
-   IntegrateurEulerCromer (double dt=0);
-   virtual void evolue(ObjetMobile& obj) const override; //regler pb eq dif ordre 1 ou 2
-   virtual IntegrateurEulerCromer* copie() const override;
-   virtual std::ostream& affiche_nom(std::ostream&) const override;
-};
-
 std::ostream& operator<<(std::ostream&, Integrateur const&);
+
